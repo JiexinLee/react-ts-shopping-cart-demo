c14c66dcee200debdcd880e31d1d714fe7d0f48d
Lee <ljxxx1206@gmail.com>
2022-08-15 16:20:39 +0930

    apply nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Home from "./pages/Home";
 import Store from "./pages/Store";
 import About from "./pages/About";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Container>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/" element={<Store />}></Route>
-        <Route path="/" element={<About />}></Route>
-      </Routes>
-    </Container>
+    <Fragment>
+      <NavBar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/store" element={<Store />}></Route>
+          <Route path="/about" element={<About />}></Route>
+        </Routes>
+      </Container>
+    </Fragment>
   );
 }
 
