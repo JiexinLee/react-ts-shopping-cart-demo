f50f3a0421fce1fbe7ae7da345e8d77851eb2a6d
Lee <ljxxx1206@gmail.com>
2022-08-29 16:45:58 +0930

    Create reminder page.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Reminder } from "../models/Reminders";
+import ReminderList from "./ReminderList";
+
+const reminders: Reminder[] = [
+  {
+    id: 1,
+    title: "Reminder 1",
+  },
+];
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <ReminderList items={reminders} />
+    </div>
+  );
 }
 
 export default Home;
